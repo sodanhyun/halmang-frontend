75c7d6227dd60851ae39c3a25a6638ae73c91da6
jiohjung98 <ckaquz98@naver.com>
2024-12-12 21:20:14 +0900

    design(send): card default button color

--- src/component/GreetingSendButton.tsx
+++ src/component/GreetingSendButton.tsx
@@ -2,18 +2,22 @@ import React from "react";
 
 interface GreetingSendButtonProps {
     onClick: () => void;
+    isDisabled: boolean;
 }
 
-const GreetingSendButton: React.FC<GreetingSendButtonProps> = ({ onClick }) => {
+const GreetingSendButton: React.FC<GreetingSendButtonProps> = ({ onClick, isDisabled }) => {
     return (
         <button
             onClick={onClick}
-            className="w-full h-[62px] bg-illustration-yellow hover:bg-blue-950 active:bg-blue-950 rounded-[20px] flex justify-center items-center gap-2.5 transition-colors duration-200"
+            disabled={isDisabled}
+            className={`w-full h-[62px] rounded-[20px] flex justify-center items-center gap-2.5 transition-colors duration-200 ${isDisabled
+                ? "bg-gray-200 cursor-not-allowed"
+                : "bg-illustration-yellow hover:bg-blue-950 active:bg-blue-950"
+                }`}
         >
             <div className="text-center text-blue-50 text-body3Bold font-['Pretendard']">
                 안부 보내기
             </div>
-
         </button>
     );
 };
--- src/page/SendPage.tsx
+++ src/page/SendPage.tsx
@@ -3,7 +3,6 @@ import ImageSelector from "../component/ImageSelector";
 import GreetingSendButton from "../component/GreetingSendButton";
 import MessageIcon from "../../static/images/Message.svg";
 
-
 const SendPage = () => {
   const [selectedCardId, setSelectedCardId] = useState<number | null>(null);
 
@@ -19,12 +18,15 @@ const SendPage = () => {
   return (
     <div className="flex flex-col items-center justify-between w-full h-full">
       <div className="flex items-center justify-center mt-6 mb-6 text-body1SemiBold text-primary-brown-950">
-        <span className="mr-2">어떤 안부를 전할까요?</span>
+        <span className="mr-[8px]">어떤 안부를 전할까요?</span>
         <img src={MessageIcon} alt="Message Icon" className="w-5 h-5" />
       </div>
       <ImageSelector onSelect={setSelectedCardId} />
       <div className="mt-auto w-full">
-        <GreetingSendButton onClick={handleSendClick} />
+        <GreetingSendButton
+          onClick={handleSendClick}
+          isDisabled={selectedCardId === null}
+        />
       </div>
     </div>
   );
