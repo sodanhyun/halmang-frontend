cca8fa04afa11cfdfb08ed8bf472035fc9a6934a
jiohjung98 <ckaquz98@naver.com>
2024-12-12 15:55:05 +0900

    design(button): home button design

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,6 +1,13 @@
+import GreetingButton from "../component/GreetingButton";
+
 const HomePage = () => {
+  const handleButtonClick = () => {
+    console.log("click");
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
+      {/* <GreetingButton onClick={handleButtonClick} /> */}
       <h1>Home Page</h1>
     </div>
   );
