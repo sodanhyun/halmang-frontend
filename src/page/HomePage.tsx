b90fcb078f4546ca5c9323697142038fae74321b
jiohjung98 <ckaquz98@naver.com>
2024-12-13 02:43:17 +0900

    feat: main card 조건부 렌더링

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -5,6 +5,8 @@ import GreetingConfirmButton from "../component/GreetingConfirmButton";
 import { getEmojiCount, getUnreadEmojis, markEmojiAsRead } from "../api/emoji";
 import { EmojiReadResponse } from "../type/emoji";
 import { emojiMap } from "../constants";
+import noHistoryImage from "../../static/images/no_history.svg";
+import readAllEmojiImage from "../../static/images/read_all_emoji.svg";
 // import ToastPopup from "../component/ToastPopup";
 
 const HomePageCard = ({ src, alt }: { src: string; alt: string }) => {
@@ -20,16 +22,47 @@ interface HomePageCardStackProps {
   setEmojis: React.Dispatch<SetStateAction<EmojiReadResponse[] | undefined>>;
 }
 
-const HomePageCardStack: React.FC<HomePageCardStackProps> = ({ emojis, setEmojis }) => {
+
+const HomePageCardStack: React.FC<HomePageCardStackProps & { count: number }> = ({
+  emojis,
+  setEmojis,
+  count,
+}) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
+    setIsLoading(true);
     getUnreadEmojis()
       .then((res) => {
         setEmojis(res);
       })
       .catch((error) => {
         console.error("Error fetching unread emojis:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [setEmojis]);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (count === 0) {
+    return (
+      <div className="flex items-center justify-center h-[426px]">
+        <img src={noHistoryImage} alt="No History" className="w-[240px] h-[296px]" />
+      </div>
+    );
+  }
+
+  if (count > 0 && (!emojis || emojis.length === 0)) {
+    return (
+      <div className="flex items-center justify-center h-[426px]">
+        <img src={readAllEmojiImage} alt="Read All Emoji" className="w-[240px] h-[296px]" />
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center justify-center">
@@ -65,13 +98,27 @@ const HomePage = () => {
       });
   };
 
+  const buttonText =
+    count === 0 || (count > 0 && (!emojis || emojis.length === 0))
+      ? "안부 보내기"
+      : "안부 확인하기";
+
+  const onNavigate =
+    count === 0 || (count > 0 && (!emojis || emojis.length === 0))
+      ? () => { }
+      : undefined;
+
   return (
     <div className="flex flex-col h-full">
       <Banner count={count} />
       <h1 className="flex items-center h-[58px] text-primary-brown-950 text-heading1Bold">오늘 받은 안부</h1>
-      <HomePageCardStack emojis={emojis} setEmojis={setEmojis} />
+      <HomePageCardStack emojis={emojis} setEmojis={setEmojis} count={count} />
       <div className="mt-auto mb-[24px]">
-        <GreetingConfirmButton onClick={handleButtonClick} />
+        <GreetingConfirmButton
+          onClick={handleButtonClick}
+          buttonText={buttonText}
+          onNavigate={onNavigate}
+        />
       </div>
     </div>
   );
