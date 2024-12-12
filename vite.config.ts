79335a2b9351310694cd887cb35f794305cd8bf5
sodanhyun <sodanhyun@gmail.com>
2024-12-12 22:52:05 +0000

    t

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync("localhost-key.pem"),
-      cert: fs.readFileSync("localhost.pem"),
-    },
-    host: "172.30.1.84",
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
